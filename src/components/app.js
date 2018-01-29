2d3dd7f9daf30f51322274d085af33f354c2cdcd
Dominik Huber <dhuber666@gmail.com>
2018-01-29 19:47:48 +0100

    bug fixes

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import SearchBar from './search-bar';
 import VideoDetail from './video-detail';
-import YTsearch from 'youtube-search-api-with-axios';
+// import YTsearch from 'youtube-search-api-with-axios';
+import YTsearch from 'youtube-api-search';
 import VideoList from './video-list';
+import _ from 'lodash';
 
 
 const API_KEY = 'AIzaSyAf6JZcUa6O_xjHxSAEb-5cBAUnmb2KE0g';
@@ -12,32 +14,36 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       videos: [],
       selectedVideo: null
     };
 
     this.searchYoutube('drums');
-    
+
   }
 
   searchYoutube = (term) => {
-    console.log(term);
-    YTsearch({key: API_KEY, term: term, maxResults: 5}, (videos) => {
-      this.setState({videos: videos, selectedVideo: videos[0]})
+
+    YTsearch({ key: API_KEY, term: term }, (videos) => {
+      console.log(videos[0].snippet.title);
+
+      this.setState({ videos: videos, selectedVideo: videos[0] })
     });
   }
 
+  searchYoutubeDebounced = _.debounce(this.searchYoutube, 300)
+
   selectVideo = (selectedVideo) => {
-    this.setState({selectedVideo})
+    this.setState({ selectedVideo })
   }
 
   render() {
     return (
       <div>
-        <SearchBar search={this.searchYoutube}/>
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos} selectVideo={this.selectVideo}/>
+        <SearchBar search={this.searchYoutubeDebounced} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} selectVideo={this.selectVideo} />
       </div>
     );
   }
